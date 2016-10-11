c58c624fa4fb9ecaab779ee901847552799e5328
Leonardo Rocha <leo_tap_metal@hotmail.com>
2016-10-10 21:46:15 -0300

    Integração de autenticacao para tela novo atleta

--- Academia-Project/src/main/java/br/com/LeonardoMatheus/controller/SegurancaController.java
+++ Academia-Project/src/main/java/br/com/LeonardoMatheus/controller/SegurancaController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SegurancaController {
 	
+	//redirect
 	@RequestMapping("/login")
 	public String login (@AuthenticationPrincipal User user){
 		if (user != null) {
