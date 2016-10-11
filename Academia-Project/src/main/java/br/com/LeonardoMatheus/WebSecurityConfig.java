0476b32920e5ad39c3083f35c797bcb6051717dd
Leonardo Rocha <leo_tap_metal@hotmail.com>
2016-10-10 22:36:47 -0300

    Implementação de Logout na tela atleta/novo

--- Academia-Project/src/main/java/br/com/LeonardoMatheus/WebSecurityConfig.java
+++ Academia-Project/src/main/java/br/com/LeonardoMatheus/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
@@ -14,7 +15,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication()
-			.withUser("joao").password("joao").roles("CADASTRAR_ATLETA");
+			.withUser("joao").password("joao").roles("CADASTRAR_ATLETA")
+			.and()
+			.withUser("maria").password("maria").roles("C");
 	}
 	
 	@Override
@@ -36,6 +39,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 						.loginPage("/login").permitAll()
 						.and()
 				.logout()
-						.permitAll();
+				.permitAll()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+						
+		
 	}
 }
