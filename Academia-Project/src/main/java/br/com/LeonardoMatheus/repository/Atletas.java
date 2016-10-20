1b54185b5d4659c42633e0965da66b397e491766
Leonardo Rocha <leo_tap_metal@hotmail.com>
2016-10-19 23:00:12 -0200

    # Organizando a estrutura do projeto

--- Academia-Project/src/main/java/br/com/LeonardoMatheus/repository/Atletas.java
+++
@@ -1,9 +0,0 @@
-package br.com.LeonardoMatheus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.LeonardoMatheus.model.AtletaModel;
-
-public interface Atletas extends JpaRepository<AtletaModel,Integer>{
-
-}
