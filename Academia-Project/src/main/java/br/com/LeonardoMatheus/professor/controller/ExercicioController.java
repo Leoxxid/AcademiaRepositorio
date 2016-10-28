41fd04cfbdb1768a516a1f51feec893df8ea7b5b
Leonardo Rocha <leo_tap_metal@hotmail.com>
2016-10-28 01:55:43 -0200

    #Get e Post- Exercicio

--- Academia-Project/src/main/java/br/com/LeonardoMatheus/professor/controller/ExercicioController.java
+++ Academia-Project/src/main/java/br/com/LeonardoMatheus/professor/controller/ExercicioController.java
@@ -1,23 +1,43 @@
 package br.com.LeonardoMatheus.professor.controller;
 
+import org.springframework.validation.Validator;
+import java.util.jar.Attributes;
+
+import javax.validation.Valid;
+
+import org.apache.catalina.filters.AddDefaultCharsetFilter;
+import org.aspectj.weaver.bcel.AtAjAttributes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.LeonardoMatheus.professor.model.treinoAtleta.ExercicioModel;
+import br.com.LeonardoMatheus.professor.service.ExercicioService;
 
 @Controller
 @RequestMapping("/exercicio")
 public class ExercicioController {
 
-	@RequestMapping(value = "/novo", method = RequestMethod.GET)
-	private String novoExercicioG() {
+	@Autowired
+	public ExercicioService service;
 
-		return "/layout/professor/cadastroExercicio";
+	@RequestMapping(value = "/novo", method = RequestMethod.GET)
+	private ModelAndView novoExercicioG(ExercicioModel exercicioM) {
+		return new ModelAndView("/layout/professor/cadastroExercicio");
 	}
 
 	@RequestMapping(value = "/novo", method = RequestMethod.POST)
-	private String novoExercicioP() {
-		return "layout/professor/cadastroExercicio";
+	private ModelAndView novoExercicioP(@Valid ExercicioModel exercicioModel, BindingResult bindingResult,
+			RedirectAttributes attributes) {
+		if (bindingResult.hasErrors()) {
+			return novoExercicioG(exercicioModel);
+		}
+		service.save(exercicioModel);
+		attributes.addFlashAttribute("sucesso", "O exercicio foi salvo com sucesso");
+		return new ModelAndView("redirect:/exercicio/novo");
 	}
 }
--- Academia-Project/src/main/java/br/com/LeonardoMatheus/professor/service/ExercicioService.java
+++ Academia-Project/src/main/java/br/com/LeonardoMatheus/professor/service/ExercicioService.java
@@ -1,5 +1,17 @@
 package br.com.LeonardoMatheus.professor.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.LeonardoMatheus.professor.model.treinoAtleta.ExercicioModel;
+import br.com.LeonardoMatheus.professor.repository.Exercicio;
+
+@Service
 public class ExercicioService {
+	@Autowired
+	public Exercicio exercico;
+	public void save(ExercicioModel exercicioModel){
+		this.exercico.save(exercicioModel);
+	}
 
 }
