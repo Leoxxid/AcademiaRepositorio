7461deaad89cbff14d53e1d410d3427f6dad0a73
matheus <madriano210@hotmail.com>
2016-11-02 17:23:47 -0200

    -Criado verTreino e index para atleta;
    -Melhorados buttons das index's

---
+++ Academia-Project/src/main/java/br/com/LeonardoMatheus/atleta/controller/atletaController.java
@@ -0,0 +1,28 @@
+package br.com.LeonardoMatheus.atleta.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import br.com.LeonardoMatheus.professor.repository.Atletas;
+import br.com.LeonardoMatheus.professor.service.AtletaService;
+
+@Controller
+@RequestMapping("/atleta")
+public class atletaController {
+
+	@Autowired
+	public Atletas atleta;
+
+	@Autowired
+	public AtletaService service;
+
+	@RequestMapping("/atleta/estatisticas")
+	public String estatisticaAtleta(){
+		return "/layout/atleta/estatisticas";
+	}
+	
+	@RequestMapping("/atleta/treino")
+	public String treinoAtleta(){
+		return "/layout/atleta/verTreino";
+	}
+}
